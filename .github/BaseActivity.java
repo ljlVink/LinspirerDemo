0c8a39e323727bb38dc9b16654b74c14d7af7895
ljlVink <1666217358@qq.com>
2023-10-18 22:35:40 +0800

    fix actions

--- .github/BaseActivity.java
+++ .github/BaseActivity.java
@@ -1,4 +1,4 @@
-package com.ljlVink.Activity;
+package com.ljlVink.lsphunter;
 
 import android.graphics.drawable.Drawable;
 import android.view.View;
@@ -6,40 +6,30 @@ import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.SimpleTarget;
 import com.bumptech.glide.request.transition.Transition;
-import com.gyf.immersionbar.ImmersionBar;
 import com.huosoft.wisdomclass.linspirerdemo.R;
 import com.ljlVink.core.core.Postutil;
-import com.ljlVink.utils.DataUtils;
-import com.ljlVink.utils.Sysutils;
+import com.ljlVink.lsphunter.utils.DataUtils;
+import com.ljlVink.lsphunter.utils.Sysutils;
 
 import java.io.File;
 
-public class BaseActivity extends AppCompatActivity {
-    public void initview(){
-        ImmersionBar.with(this).transparentStatusBar().init();
-        try {
-            getSupportActionBar().hide();
-        } catch (Exception e) {
-        }
-    }
-    public void setvisibility(boolean isVisible){
+public class BaseActivity extends FragmentActivity {
+    public void Setvisibility(boolean isVisible) {
         isVisible=true;
-        findViewById(R.id.titlebar).setVisibility(View.VISIBLE);
-        new Postutil(this).SwordPlan();
+        findViewById(R.id.titlebar2).setVisibility(View.VISIBLE);
         try{
             if (isVisible) {
-                findViewById(R.id.left).setVisibility(View.VISIBLE);
+                findViewById(R.id.left_container).setVisibility(View.VISIBLE);
                 if(Sysutils.isTabletDevice(this))
-                    findViewById(R.id.right).setVisibility(View.VISIBLE);
-                findViewById(R.id.grid_photo).setVisibility(View.VISIBLE);
+                    findViewById(R.id.right_container).setVisibility(View.VISIBLE);
                 String bgpath= DataUtils.readStringValue(this,"background_bg","");
                 if(!bgpath.equals("")&&new File(bgpath).exists()){
-                    LinearLayout ll=findViewById(R.id.background_newui);
+                    LinearLayout ll=findViewById(R.id.background_lsphunter);
                     Glide.with(this)
                             .load(bgpath)
                             .into(new SimpleTarget<Drawable>() {
@@ -51,10 +41,8 @@ public class BaseActivity extends AppCompatActivity {
                 }
                 onResume();
             } else {
-                findViewById(R.id.left).setVisibility(View.VISIBLE);
-                if(Sysutils.isTabletDevice(this))
-                    findViewById(R.id.right).setVisibility(View.VISIBLE);
-                findViewById(R.id.grid_photo).setVisibility(View.INVISIBLE);
+                findViewById(R.id.left_container).setVisibility(View.INVISIBLE);
+                findViewById(R.id.right_container).setVisibility(View.VISIBLE);
                 onResume();
             }
         }catch (Exception e){
@@ -62,6 +50,13 @@ public class BaseActivity extends AppCompatActivity {
         }catch (Throwable th){
 
         }
+    }
+    public void setfalseVisibility(){
+        try{
+            findViewById(R.id.titlebar2).setVisibility(View.INVISIBLE);
+            findViewById(R.id.left_container).setVisibility(View.INVISIBLE);
+            if(Sysutils.isTabletDevice(getApplicationContext())) findViewById(R.id.right_container).setVisibility(View.INVISIBLE);
+        }catch (Exception e){}
     }
 
 }
